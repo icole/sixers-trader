d845758d6cf62901b4240d1ccf2aef2eec5f2408
Ian Cole <ian.cole@me.com>
2018-01-24 21:02:04 -0800

    Serve build directory

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'src')))
+  .use(express.static(path.join(__dirname, 'build', 'contracts')))
   .get('/', (req, res) => res.render(path.join(__dirname, 'src/index.html')))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
